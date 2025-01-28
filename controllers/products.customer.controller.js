736bbe66b1b7baead11a7d4d98cec4416e6f3608
Sara Salem <sarasalem9898@outlook.com>
2025-01-28 23:28:45 +0200

    change paginated products

--- controllers/products.customer.controller.js
+++ controllers/products.customer.controller.js
@@ -1,5 +1,5 @@
-const productService=require('../services/product.service');
-const orderservice=require("../services/order.service");
+const productService = require('../services/product.service');
+const orderservice = require("../services/order.service");
 const { unifiedResponse, handleError } = require('../utils/responseHandler');
 
 module.exports = (() => {
@@ -7,24 +7,37 @@ module.exports = (() => {
 
     router.get("/", async (req, res, next) => {
         try {
-            // Validate request data
-            // const { error, value } = createclerkDto.validate(req.body,{abortEarly:false});
-            // if (error) {
-            //     const errors= error.details.map(e=>e.message);
-            //     return res.status(400).json(unifiedResponse(400, 'Validation Error', errors));
-            // }
+            if (req.query.page) {
+                var page = parseInt(req.query.page) || 1;
+                var limit = 6;
+                const result = await productService.getPaginatedActiveProductsService(page, limit);
+                return res.status(201).json(unifiedResponse(201, 'paginated products returned succesfully', result));
+            } else {
+                const products = await productService.getProducts();
+                return res.status(201).json(unifiedResponse(201, 'all Products returned successfully', products));
+            }
 
-            // Service call
-            
-            const products = await productService.getProducts()
-            return res.status(201).json(unifiedResponse(201, 'Products found successfully', products));
         } catch (err) {
             handleError(res, err);
         }
     })
+
+
+    //paginated products
+    router.get("/", async (req, res) => {
+        try {
+            var page = parseInt(req.query.page) || 1;
+            var limit = 6;
+            const result = await ProductService.getPaginatedActiveProductsService(page, limit);
+            return res.status(201).json(unifiedResponse(201, 'products returned succesfully', result));
+        } catch (err) {
+            handleError(res, err);
+        }
+    })
+
     router.get("/:status", async (req, res, next) => {
         try {
-            const status=req.params.status;
+            const status = req.params.status;
             const products = await productService.getproductsbyStatus(status)
             return res.status(201).json(unifiedResponse(201, 'Products retrived successfully', products));
         } catch (err) {
@@ -33,23 +46,20 @@ module.exports = (() => {
     })
     router.patch("/delete/:id", async (req, res, next) => {
         try {
-            const productid=req.params.id;
-              const orders=await orderservice.getorderbyproductid(productid);
-                                  if(orders)
-                                  {
-                                    const undeliveredOrders = orders.filter((o) =>o.product.
-                                    some((p) => p.product_id === productid && p.status !== "delivered" && p.status !== "cancelled" ));
-                                        if (undeliveredOrders.length > 0) {
-                                        return res.status(403).json(unifiedResponse(403, "Cannot deactivate product; undelivered orders exist.")); 
-                                        }
-                                  }
+            const productid = req.params.id;
+            const orders = await orderservice.getorderbyproductid(productid);
+            if (orders) {
+                const undeliveredOrders = orders.filter((o) => o.product.
+                    some((p) => p.product_id === productid && p.status !== "delivered" && p.status !== "cancelled"));
+                if (undeliveredOrders.length > 0) {
+                    return res.status(403).json(unifiedResponse(403, "Cannot deactivate product; undelivered orders exist."));
+                }
+            }
             const products = await productService.softdeleteproduct(productid);
-            if(products)
-            {
+            if (products) {
                 return res.status(201).json(unifiedResponse(201, 'Products deactive successfully', products));
             }
-            else
-            {
+            else {
                 return res.status(403).json(unifiedResponse(403, 'Product not found '));
             }
         } catch (err) {
@@ -58,14 +68,12 @@ module.exports = (() => {
     })
     router.patch("/restore/:id", async (req, res, next) => {
         try {
-            const id=req.params.id;
+            const id = req.params.id;
             const products = await productService.restoreproduct(id);
-            if(products)
-            {
+            if (products) {
                 return res.status(201).json(unifiedResponse(201, 'Products active successfully', products));
             }
-            else
-            {
+            else {
                 return res.status(403).json(unifiedResponse(403, 'Product not found '));
             }
         } catch (err) {
@@ -74,21 +82,19 @@ module.exports = (() => {
     })
     router.get("/byid/:id", async (req, res, next) => {
         try {
-            const id=req.params.id;
+            const id = req.params.id;
             const products = await productService.getProductbyid(id);
-            if(products)
-            {
+            if (products) {
                 return res.status(201).json(unifiedResponse(201, 'Products retrive successfully', products));
             }
-            else
-            {
+            else {
                 return res.status(403).json(unifiedResponse(403, 'Product not found '));
             }
         } catch (err) {
             handleError(res, err);
         }
     })
-    
+
     return router;
 
 })()
