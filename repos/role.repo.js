abfbb0ee233d17ef021ea78500d8b017a80d6516
Sara Salem <sarasalem9898@outlook.com>
2025-02-08 22:50:54 +0200

    add role

--- repos/role.repo.js
+++ repos/role.repo.js
@@ -5,4 +5,18 @@ const getPermissions=async(role_id)=>{
    return result;
 }
 
-module.exports={getPermissions}
+const getAllRoles=async()=>{
+    var result= await roleModel.find({});
+    return result;
+}
+
+const getAllPaginatedRoles = async (page, limit) => {
+    const skip = (page - 1) * limit;
+    return await roleModel.find().skip(skip).limit(limit).exec();
+};
+
+const countAllRoles = async () => {
+    return await roleModel.countDocuments();
+};
+
+module.exports={getPermissions,getAllRoles,getAllPaginatedRoles,countAllRoles}
